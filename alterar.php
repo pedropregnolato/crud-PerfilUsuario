92c28f0f4553bfe371d0f261d4afdfb33fefe7d3
Pedro Lucas Nunes Pregnolato <pregnolato.pedro@NEVESCOSTA.local>
2022-02-21 14:27:40 -0300

    front end feito e correções pendentes - script e tratamentos

--- alterar.php
+++ alterar.php
@@ -6,16 +6,18 @@ include_once './conexao.php';
 if (isset($_POST['editar_perfil'])) {
     $editar_id = $_POST['editar_id'];
     $editar_nome = $_POST['editar_nome'];
-    $editar_foto = $_FILES['foto']['name'];
-
     $editar_nome = strtoupper($editar_nome);
+    $editar_documento = $_POST['editar_documento'];
+    $editar_telefone = $_POST['editar_telefone'];
+    $editar_email = $_POST['editar_email'];
+    $foto = $_FILES['foto']['name'];
 
     if (file_exists("fotos/" . $editar_id . '/' . $_FILES["foto"]["name"])) {
         $valida_foto = $_FILES['foto']['name'];
         $_SESSION['status'] = "Foto já existente '.$valida_foto.'";
         header('Location: index.php');
     } else {
-        $query = "UPDATE cadastro SET nome='$editar_nome', foto='$editar_foto' WHERE id='$editar_id'";
+        $query = "UPDATE cadastro SET nome='$editar_nome', documento='$editar_documento', telefone='$editar_telefone', email='$editar_email', foto='$editar_foto' WHERE id='$editar_id'";
         $executar = mysqli_query($conn, $query);
 
         if ($executar) {
--- cadastrar.php
+++ cadastrar.php
@@ -1,19 +1,64 @@
-<?php
+﻿<?php
 session_start();
 include_once './conexao.php';
 
 ?>
-<form action="cadastrar.php" method="POST" enctype="multipart/form-data">
-    <p>Nome: <input type="text" name="nome" placeholder="Digite o Nome" maxlength="50" required></p>
-    <p>Clique para <input type="file" name="foto" required></p>
-    <p>Documento: <input type="tel" name="documento" placeholder=" .  .  -" maxlength="14" required></p>
-    <p>Telefone: <input type="tel" name="telefone" placeholder="() - " maxlength="14" required></p>
-    <p>Email: <input type="email" name="email" placeholder="example@mail.com" maxlength="50" required></p>
-    <input type="submit" name="cadastrar" value="Cadastrar"><br>
-</form>
 
-<button><a href="index.php">voltar</a></button>
-</body>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <title>Criação de cadastro</title>
+</head>
+
+<body>
+    <header style="margin-bottom: 3em">
+        <nav class="navbar navbar-dark bg-dark justify-content-between">
+            <a class="navbar-brand" href="#">Cadastrando Perfil</a>
+            <a href="index.php" class="btn btn-outline-danger">voltar</a>
+        </nav>
+    </header>
+    <main align="left" style="margin-left: 3em">
+        <form action="cadastrar.php" method="POST" enctype="multipart/form-data">
+            <h4>Informações</h4>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label>Nome</label><input type="text" name="nome" placeholder="Digite o Nome" maxlength="50" class="form-control" required>
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Documento</label><input type="tel" name="documento" placeholder=" .  .  -" maxlength="14" class="form-control" id="documento" required>
+                </div>
+            </div>
+            <br>
+
+            <div class="mb-3">
+                <label for="foto" class="form-label">Escolha sua foto de perfil</label>
+                <input class="form-control form-control-sm col-md-4" id="formFileSm" type="file" name="foto">
+            </div>
+
+            <br>
+            <h4>Contatos</h4>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label>Email</label><input type="email" name="email" placeholder="example@mail.com" maxlength="50" class="form-control" required>
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Telefone </label><input type="tel" name="telefone" placeholder="() - " maxlength="14" class="form-control" id="telefone" required>
+                </div>
+            </div>
+            <input type="submit" name="cadastrar" value="Cadastrar" class="btn btn-success"><br>
+        </form>
+    </main>
+
 <?php
 
 //insert
@@ -56,3 +101,17 @@ if (isset($_POST['cadastrar'])) {
         header('Location: cadastrar.php');
     }
 }
+?>
+
+
+    <script>
+        $("#documento").mask("999.999.999-99");
+        if (($("#telefone").length) === 11) {
+            $("#telefone").mask("(99)99999-9999");
+        } else if ((telefone.length) === 10) {
+            $("#telefone").mask("(99) 9999-9999");
+        }
+    </script>
+</body>
+
+</html>

06cd5117a5f9970b0fccec301df143836e6885ef
Pedro Lucas Nunes Pregnolato <pregnolato.pedro@NEVESCOSTA.local>
2022-02-22 13:56:17 -0300

    atualizando o código - alterações na tela de cadastro e inserção do template do bd

--- alterar.php
+++ alterar.php
@@ -5,28 +5,10 @@ include_once './conexao.php';
 //update
 if (isset($_POST['editar_perfil'])) {
     $editar_id = $_POST['editar_id'];
-    $editar_nome = $_POST['editar_nome'];
-    $editar_nome = strtoupper($editar_nome);
-    $editar_documento = $_POST['editar_documento'];
-    $editar_telefone = $_POST['editar_telefone'];
+    $editar_nome = strtoupper(preg_replace("/[^a-zA-Z ]+/", "", $_POST['editar_nome']));
+    $editar_documento = preg_replace("/[^0-9]/", "", $_POST['editar_documento']);
+    $editar_telefone = preg_replace("/[^0-9]/", "", $_POST['editar_telefone']);
     $editar_email = $_POST['editar_email'];
-    $foto = $_FILES['foto']['name'];
+    $editar_foto = $_FILES['foto']['name'];
 
-    if (file_exists("fotos/" . $editar_id . '/' . $_FILES["foto"]["name"])) {
-        $valida_foto = $_FILES['foto']['name'];
-        $_SESSION['status'] = "Foto já existente '.$valida_foto.'";
-        header('Location: index.php');
-    } else {
-        $query = "UPDATE cadastro SET nome='$editar_nome', documento='$editar_documento', telefone='$editar_telefone', email='$editar_email', foto='$editar_foto' WHERE id='$editar_id'";
-        $executar = mysqli_query($conn, $query);
-
-        if ($executar) {
-            move_uploaded_file($_FILES['foto']['tmp_name'], 'fotos/' . $editar_id . '/' . $_FILES['foto']['name']);
-            $_SESSION['success'] = "Cadastro atualizado!";
-            header('Location: index.php');
-        } else {
-            $_SESSION['status'] = "Ocorreu um erro. Cadastro não atualizado!";
-            header('Location: index.php');
-        }
-    }
 }
--- cadastrar.php
+++ cadastrar.php
@@ -17,6 +17,9 @@ include_once './conexao.php';
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+
     <title>Criação de cadastro</title>
 </head>
 
@@ -35,82 +38,91 @@ include_once './conexao.php';
                     <label>Nome</label><input type="text" name="nome" placeholder="Digite o Nome" maxlength="50" class="form-control" required>
                 </div>
                 <div class="form-group col-md-2">
-                    <label>Documento</label><input type="tel" name="documento" placeholder=" .  .  -" maxlength="14" class="form-control" id="documento" required>
+                    <label>Documento</label><input type="text" name="documento" placeholder=" .  .  -" maxlength="14" class="form-control" id="documento" required>
                 </div>
             </div>
             <br>
 
             <div class="mb-3">
                 <label for="foto" class="form-label">Escolha sua foto de perfil</label>
-                <input class="form-control form-control-sm col-md-4" id="formFileSm" type="file" name="foto">
+                <input class="input-group-text" type="file" name="foto">
             </div>
 
             <br>
             <h4>Contatos</h4>
             <div class="form-row">
                 <div class="form-group col-md-4">
-                    <label>Email</label><input type="email" name="email" placeholder="example@mail.com" maxlength="50" class="form-control" required>
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="example@mail.com" maxlength="50" class="form-control" required>
                 </div>
                 <div class="form-group col-md-2">
-                    <label>Telefone </label><input type="tel" name="telefone" placeholder="() - " maxlength="14" class="form-control" id="telefone" required>
+                    <label>Telefone </label>
+                    <input type="tel" name="telefone" placeholder="() - " maxlength="16" class="form-control" id="telefone" onkeypress="mascaraTelefone()" required>
                 </div>
             </div>
             <input type="submit" name="cadastrar" value="Cadastrar" class="btn btn-success"><br>
         </form>
     </main>
 
-<?php
+    <?php
 
-//insert
-if (isset($_POST['cadastrar'])) {
-    $nome = $_POST['nome'];
-    $nome = strtoupper($nome);
-    $documento = $_POST['documento'];
-    $telefone = $_POST['telefone'];
-    $email = $_POST['email'];
-    $foto = $_FILES['foto']['name'];
+    //insert
+    if (isset($_POST['cadastrar'])) {
+        $nome = strtoupper(preg_replace("/[^a-zA-Z ]+/", "", $_POST['nome']));
+        $documento = preg_replace("/[^0-9]/", "", $_POST['documento']);
+        $telefone = preg_replace("/[^0-9]/", "", $_POST['telefone']);
+        $email = $_POST['email'];
+        $foto = $_FILES['foto']['name'];
 
-    if (!empty($nome) && !empty($documento) && !empty($telefone) && !empty($email) && !empty($foto)) {
-        $query = "INSERT INTO cadastro(nome, documento, telefone, email, foto) VALUES (:nome, :documento, :telefone, :email, :foto)";
-        $executar = $conexao->prepare($query);
-        $executar->bindParam(':nome', $nome);
-        $executar->bindParam(':documento', $documento);
-        $executar->bindParam(':telefone', $telefone);
-        $executar->bindParam(':email', $email);
-        $executar->bindParam(':foto', $foto);
+        if (!empty($nome) && !empty($documento) && !empty($telefone) && !empty($email) && !empty($foto)) {
+            $query = "INSERT INTO cadastro(nome, documento, telefone, email, foto) VALUES (:nome, :documento, :telefone, :email, :foto)";
+            $executar = $conexao->prepare($query);
+            $executar->bindParam(':nome', $nome);
+            $executar->bindParam(':documento', $documento);
+            $executar->bindParam(':telefone', $telefone);
+            $executar->bindParam(':email', $email);
+            $executar->bindParam(':foto', $foto);
 
-        $return = $executar->execute();
-        if ($return) {
+            $return = $executar->execute();
+            if ($return) {
 
-            $salvar_id = $conexao->lastInsertId();
+                $salvar_id = $conexao->lastInsertId();
 
-            $diretorio = 'fotos/' . $salvar_id . "/";
+                $diretorio = 'fotos/' . $salvar_id . "/";
 
-            mkdir($diretorio, 0755);
+                mkdir($diretorio, 0755);
 
-            if (move_uploaded_file($_FILES['foto']['tmp_name'], $diretorio . $_FILES['foto']['name'])) {
-                $_SESSION['success'] = "Cadastro realizado!";
+                if (move_uploaded_file($_FILES['foto']['tmp_name'], $diretorio . $_FILES['foto']['name'])) {
+                    $_SESSION['success'] = "Cadastro realizado!";
+                    header('Location: index.php');
+                }
+            } else {
+                $_SESSION['status'] = "Ocorreu um erro. Cadastro não realizado!";
                 header('Location: index.php');
             }
         } else {
-            $_SESSION['status'] = "Ocorreu um erro. Cadastro não realizado!";
-            header('Location: index.php');
+            $_SESSION['status'] = "Preencha todos os campos!";
+            header('Location: cadastrar.php');
         }
-    } else {
-        $_SESSION['status'] = "Preencha todos os campos!";
-        header('Location: cadastrar.php');
     }
-}
-?>
-
+    ?>
 
     <script>
-        $("#documento").mask("999.999.999-99");
-        if (($("#telefone").length) === 11) {
-            $("#telefone").mask("(99)99999-9999");
-        } else if ((telefone.length) === 10) {
-            $("#telefone").mask("(99) 9999-9999");
+        function mascaraTelefone() {
+            var numero = document.getElementById("telefone").value;
+             /* alert(numero.length);  */
+            if (numero.length == 11) {
+                $("#telefone").mask("(99) 9 9999-9999");
+            } else if (numero.length == 10) {
+                $("#telefone").mask("(99) 9999-9999");
+            }
         }
+
+        $("#documento").mask("999.999.999-99");
+
+
+        /* mudar isso para regex - está dando muito erro */
+
     </script>
 </body>
 
